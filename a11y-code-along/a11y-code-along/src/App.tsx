adffbabe773744dd33722ca4007fe1db9999e952
Dougal Graham <dougalg@gmail.com>
2023-01-10 07:55:32 +0700

    Refactor headings

--- a11y-code-along/a11y-code-along/src/App.tsx
+++ a11y-code-along/a11y-code-along/src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
-import { Heading } from './components/Heading';
+import { Heading } from './components/Heading/Heading';
+import { HeadingSection } from './components/Heading/HeadingSection';
 
 function App() {
 	return (
-		<div className="App">
+		<HeadingSection tag="div" className="App">
 			<header>
-				<Heading level={1}>Minerva</Heading>
-				<Heading level={2} id="nav-header">
-					Site Navigation
-				</Heading>
+				<Heading>Minerva</Heading>
+				<HeadingSection>
+					<Heading level={2} id="nav-header">
+						Site Navigation
+					</Heading>
+				</HeadingSection>
 				<nav aria-labelledby="nav-header">
 					<a href="/">Demos</a>
 					<a href="/">Features</a>
@@ -17,18 +20,18 @@ function App() {
 				</nav>
 			</header>
 			<main>
-				<section>
-					<Heading level={2}>Popular Articles</Heading>
-				</section>
-				<section>
+				<HeadingSection tag="section">
+					<Heading>Popular Articles</Heading>
+				</HeadingSection>
+				<HeadingSection tag="section">
 					<Heading level={2}>Editor&lsquo;s Choice</Heading>
-				</section>
-				<section>
+				</HeadingSection>
+				<HeadingSection tag="section">
 					<Heading level={2}>Latest Posts</Heading>
-				</section>
+				</HeadingSection>
 			</main>
 			<footer>This is a footer</footer>
-		</div>
+		</HeadingSection>
 	);
 }
 
