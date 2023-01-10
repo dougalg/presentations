70d935960dbc2ad00b9289e3c9f2616772dcc343
Dougal Graham <dougalg@gmail.com>
2023-01-10 07:33:00 +0700

    Add clampNumber for headings

--- a11y-code-along/a11y-code-along/src/components/Heading.tsx
+++ a11y-code-along/a11y-code-along/src/components/Heading.tsx
@@ -1,9 +1,11 @@
+import { clampNumber } from '../util/number';
+
 type HeadingProps = {
 	level: number;
 	children: React.ReactNode;
 } & React.HTMLAttributes<HTMLHeadingElement>;
 
 export const Heading = ({ level, children, ...otherProps }: HeadingProps): JSX.Element => {
-	const HeadingTag = `h${level}`;
+	const HeadingTag = `h${clampNumber(1, 6, level)}`;
 	return <HeadingTag {...otherProps}>{children}</HeadingTag>;
 };
